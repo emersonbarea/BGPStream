69c86db8fb9565346c19967a8d9ba8ef4c32363f
Tocha <emerson.barea@gmail.com>
2019-05-24 09:40:38 -0300

    comment

--- install.sh
+++ install.sh
@@ -6,32 +6,57 @@
 function update_OS_install_requirements() {
     printf '\n\e[1;33m%-6s\e[m\n' '-- Updating O.S. and installing BGPStream packages requeriments...'
     sudo apt update; sudo apt upgrade -y; sudo apt autoremove
-    sudo apt install libbz2-dev zlib1g-dev libcurl-ocaml-dev build-essential dh-autoreconf python -y
+    sudo apt install libbz2-dev zlib1g-dev libcurl-ocaml-dev build-essential dh-autoreconf python python-pip -y
 }
 
 function install_wandio() {
     printf '\n\e[1;33m%-6s\e[m\n' '-- Installing Wandio-4.0.0...'
+    cd $INSTALL_DIR
     rm -rf $INSTALL_DIR/wandio-4.0.0/ 2> /dev/null
+    printf '\n\e[1;33m%-6s\e[m\n' '- downloading Wandio'
     curl -O https://research.wand.net.nz/software/wandio/wandio-4.0.0.tar.gz -P $INSTALL_DIR
     tar zxf $INSTALL_DIR/wandio-4.0.0.tar.gz
     cd $INSTALL_DIR/wandio-4.0.0/
+    printf '\n\e[1;33m%-6s\e[m\n' '- Wandio - ./configure'
     ./configure
+    printf '\n\e[1;33m%-6s\e[m\n' '- Wandio - make'
     sudo make
+    printf '\n\e[1;33m%-6s\e[m\n' '- Wandio - make install'
     sudo make install
     cd $INSTALL_DIR
+    printf '\n\e[1;33m%-6s\e[m\n' '- Wandio - ldconfig'
     sudo ldconfig
 }
 
 function install_BGPStream() {
     printf '\n\e[1;33m%-6s\e[m\n' '-- Installing BGPStream 1.2.3...'
     cd $INSTALL_DIR
+    printf '\n\e[1;33m%-6s\e[m\n' '- downloading BGPStream'
     git clone https://github.com/CAIDA/bgpstream.git
     cd $INSTALL_DIR/bgpstream
+    printf '\n\e[1;33m%-6s\e[m\n' '- BGPStream - ./autogen.sh'
+    #sed -i -- 's/git submodule update/git submodule update\nmv ..\/ltmain.sh ./g' autogen.sh
+    printf 'autoreconf -vfi' | sudo tee --append autogen.sh
     ./autogen.sh
+    printf '\n\e[1;33m%-6s\e[m\n' '- BGPStream - ./configure'
     ./configure
+    printf '\n\e[1;33m%-6s\e[m\n' '- BGPStream - make'
     sudo make
+    printf '\n\e[1;33m%-6s\e[m\n' '- BGPStream - make install'
     sudo make install
     cd $INSTALL_DIR
+}
+
+function install_PyBGPStream() {
+    printf '\n\e[1;33m%-6s\e[m\n' '-- Installing PyBGPStream...'
+    pip install pybgpstream
+}
+
+function test_PyBGPStream() {
+    printf '\n\e[1;33m%-6s\e[m\n' '-- Installing PyBGPStream...'
+    cd $INSTALL_DIR
+    ./pybgpstream-print.py
+    printf '\n\e[1;33m%-6s\e[m\n' 'If you are seeing an output started with this content "('ris', 'rrc11', 'update', 1438417216, 'valid'), everything is rigth :)'
 }
 
 USER=$(whoami)
@@ -41,3 +66,5 @@ INSTALL_DIR=$HOME_DIR/BGPStream
 update_OS_install_requirements
 install_wandio
 install_BGPStream
+install_PyBGPStream
+test_PyBGPStream
